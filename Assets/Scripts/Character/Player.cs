783fb1ba9147ed0782e1851b45a9b1a65d6bc534
DESKTOP-RGPKKI0\Jouri <jouri96@hotmail.com>
2023-07-26 01:31:48 +0200

    Fix #2

--- Assets/Scripts/Character/Player.cs
+++ Assets/Scripts/Character/Player.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Sirenix.OdinInspector;
+//using Sirenix.OdinInspector;
 using UnityEngine.UI;
 
 namespace ToBeNamed.Character
--- Assets/Scripts/Worldgen/EnemySpawner.cs
+++ Assets/Scripts/Worldgen/EnemySpawner.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Sirenix.OdinInspector;
+//using Sirenix.OdinInspector;
 
 namespace ToBeNamed.Worldgen
 {
